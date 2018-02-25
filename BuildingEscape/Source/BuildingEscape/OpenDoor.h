7ccc76b835fb7cc76683c27062b69def17d87b22
Atid Puwatnuttasit <atid.puw@gmail.com>
2018-02-25 16:48:13 +0700

    Create TriggerVolume for door interaction.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -4,13 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
-
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
 {
-	GENERATED_BODY()
+GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
@@ -24,6 +24,10 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.0f;
 	
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
 };
